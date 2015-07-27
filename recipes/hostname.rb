67dadef796c987adb88d68e18e310c420ead0a9a
José F. Romaniello <jfromaniello@gmail.com>
2015-07-27 10:14:17 -0300

    Add support for sibling nodes
    Sibling nodes are nodes that are nearer of other nodes than from the s3 bucket.
    
    This usually happens with third party VPS services.
    
    Eg
    
    ```
      "siblings": {
        "cdn-southafrica-1": "cdn-eu-central"
      }
    ```
    
    This means that the node in South Africa uses Europe Central as a backend instead of S3.
    
    If EU Central is down in the above example both nodes will be down.

--- recipes/hostname.rb
+++ recipes/hostname.rb
@@ -10,3 +10,10 @@ hostsfile_entry '127.0.1.1' do
   hostname  "#{node.name}.cdn.auth0"
   aliases   [node.name]
 end
+
+node['cxn']['nodes'].sort.each do |host, ip|
+  next if host == node.name
+  hostsfile_entry ip do
+    hostname  host
+  end
+end
