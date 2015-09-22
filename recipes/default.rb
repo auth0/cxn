baf58f8112cbd95daf0948b3bb38618077fc6595
José F. Romaniello <jfromaniello@gmail.com>
2015-09-22 15:19:25 -0300

    update kernel limits

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,7 @@ include_recipe 'cxn::nginx'
 include_recipe 'cxn::varnish'
 include_recipe 'cxn::newrelic'
 include_recipe 'cxn::hostname'
+include_recipe 'cxn::sysctl'
 
 if node['cxn']['logging'] then
     include_recipe 'cxn::stunnel'
---
+++ recipes/sysctl.rb
@@ -0,0 +1,52 @@
+include_recipe 'sysctl::default'
+
+# Netflix defaults
+# http://wiki.mikejung.biz/Sysctl_tweaks#Netflix_2014_EC2_sysctl_tweaks
+
+sysctl_param 'vm.swappiness' do
+  value 20
+end
+
+sysctl_param 'vm.dirty_background_ratio' do
+  value 10
+end
+
+sysctl_param 'vm.dirty_ratio' do
+  value 40
+end
+
+sysctl_param 'net.core.somaxconn' do
+    value 1000
+end
+
+sysctl_param 'net.core.netdev_max_backlog' do
+    value 5000
+end
+
+sysctl_param 'net.core.rmem_max' do
+    value 16777216
+end
+
+sysctl_param 'net.core.wmem_max' do
+    value 16777216
+end
+
+sysctl_param 'net.ipv4.tcp_wmem' do
+    value '4096 12582912 16777216'
+end
+
+sysctl_param 'net.ipv4.tcp_rmem' do
+    value '4096 12582912 16777216'
+end
+
+sysctl_param 'net.ipv4.tcp_max_syn_backlog' do
+    value 8096
+end
+
+sysctl_param 'net.ipv4.tcp_slow_start_after_idle' do
+    value 0
+end
+
+sysctl_param 'net.ipv4.tcp_tw_reuse' do
+    value 1
+end
